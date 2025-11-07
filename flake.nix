6e72cf949604a51d2d1532e3110ddee9695780e8
Huy-DNA <huydo862003@gmail.com>
2025-11-07 15:48:09 +0700

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "jaffle-shop on NixOS with dbt-duckdb";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+  outputs = { self, nixpkgs }:
+  let
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+    forAllSystems = f: builtins.listToAttrs (map (sys: { name = sys; value = f sys; }) systems);
+  in {
+    devShells = forAllSystems (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            dbt
+            duckdb
+            git
+            uv
+          ];
+        };
+      });
+  };
+}
